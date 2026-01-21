62db2687ff7899f8421f01e4f0d7b76c84c6820e
saimaanliikenne <sl.matkatoimisto@gmail.com>
2026-01-21 14:19:55 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Matkatoimisto Saimaan Liikenne OY";
-export const SITE_DESCRIPTION = "Matkailua, luontoa ja reittejä Suomessa";
+export const SITE_TITLE = "Matkatoimisto Saimaan Liikenne";
+export const SITE_DESCRIPTION = "Matkailua, luontoa, reittejä ja vinkkejä matkustamiseen Suomessa";
